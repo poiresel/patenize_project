e68202faa129b6d60452c4ceabfc8e6f7f242f73
Pranav Gaddamadugu <pranavsaig@gmail.com>
2017-07-22 14:41:27 -0700

    patent submission as mappings

---
+++ contracts/PatentSubmission.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.4.9;
+
+contract PatentSubmission {
+
+    //TODO: assign patents to reviewers specifically. Create a log of reviewer who approved patent
+    address patentOffice;
+    uint256 multiplicativeGroup;
+    uint256 modulo;
+    uint256 reviewerNumber;
+    uint256 applicationFee; //Optional
+    uint256 nextApp;
+    uint256 applicationCount;
+
+    mapping (uint256 => Application) apps;
+
+    struct Application {
+        bytes32 ipfsHash;
+        bool underReview;
+    }
+
+    modifier onlyOffice() {
+        if (msg.sender != patentOffice) {
+            throw;
+        }
+        _;
+    }
+
+    event PatentDecision();
+
+    function PatentSubmission(address office, uint256 group, uint256 mod, uint256 rev_number, uint256 fee) {
+        patentOffice = office;
+        multiplicativeGroup = group;
+        modulo = mod;
+        reviewerNumber = rev_number;
+        applicationFee = fee;
+        applicationCount = 0;
+    }
+
+    function decideOnPatent(bytes32 app, bool approval) onlyOffice() {
+
+    }
+
+    //TODO: make this a payable function
+    function submitPatent(bytes32 app) {
+        applications.push(app);
+        inReview[app] = false;
+        applicationCount += 1;
+    }
+
+
+}
